817d6abf84646d2f16fe4c9cd19667ac9cdae2ef
Roman Taranenko <roman@aruodas.lt>
2016-11-23 16:14:48 +0200

    Cleanup

--- client/index.php
+++ client/index.php
@@ -4,29 +4,15 @@
 
     $key = '530120e67dd417b5a8bbc324f25f04d4';
 
-    /*
-        $token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE0Nzk4OTkzMjMsImV4cCI6MTQ3OTg5OTYyM30.n97XhPDtRU6R17MqxzQK8QD01jLTnKXt6gX9hEHurDY';
 
-        try {
-            $decode = \Firebase\JWT\JWT::decode($token, $key, ['HS256']);
-            var_dump($decode);
-        } catch(\Exception $e) {
-            var_dump($e->getMessage());
-        }
-    */
-
-//    session_unset();
     if (empty($_SESSION['token'])) {
         $data = [
             'iat' => time(),
-            'name' => 'Roman',
-            'project' => 'autoplius'
+            'name' => 'Roman'
         ];
 
         $_SESSION['token'] = \Firebase\JWT\JWT::encode($data, $key);
     }
-
-    echo $_SESSION['token'];
 ?>
 
 <script src="//cdn.socket.io/socket.io-1.4.5.js"></script>
